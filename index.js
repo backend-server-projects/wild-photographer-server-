43ae6aedd34a93d9bf6aa8ccaf912ae5a1a66f03
Arman Khan <sparmankhan@gmail.com>
2022-11-09 22:13:22 +0600

    jwt token added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express= require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -15,10 +16,33 @@ app.get('/',(req,res)=>{
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tgkwl01.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verifyJWT = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 const run = async()=>{
     try{
         const servicesCollection = client.db('wildPhotographer').collection('services')
         const reviewCollection = client.db('wildPhotographer').collection('review')
+
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '4h'})
+            res.send({token})
+        })
+
+
         app.get('/services',async(req,res)=>{
             const query = {}
             const cursor = servicesCollection.find(query)
@@ -44,8 +68,7 @@ const run = async()=>{
         app.get('/details/:id', async(req,res)=>{
             const id = req.params.id
             const query = {_id: ObjectId(id)}
-            const cursor = servicesCollection.findOne(query)
-            const result = await cursor;
+            const result = await servicesCollection.findOne(query)
             res.send(result);
         }) 
 
@@ -60,15 +83,46 @@ const run = async()=>{
         app.get('/review/:id', async(req,res)=>{
             const review_id = req.params.id
             const query = {review_id}
-            const cursor = reviewCollection.find(query)
+            const cursor = reviewCollection.find(query).sort({$natural:-1})
             const review = await cursor.toArray();
             res.send(review)
         }) 
 
-        app.get('/myReview',async(req,res)=>{
-            const email = req.headers.email;
+        app.get('/edit/:id', async(req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const cursor =  reviewCollection.find(query)
+            const review = await cursor.toArray();
+            res.send(review)
+        }) 
+
+        app.put('/edit/:id', async(req,res)=>{
+            const id = req.params.id;
+            const review = req.body;
+            const filter = {_id: ObjectId(id)}
+            const option = {upsert: true}
+            const updateReview = {
+                $set:{
+                    reviewText: review.reviewText,
+                    email: review.email,
+                    fullName: review.fullName,
+                    image: review.image
+                }
+            }
+            const result = await reviewCollection.updateOne(filter,updateReview,option)
+            res.send(result)
+
+        })
+
+        app.get('/myReview',verifyJWT,async(req,res)=>{
+            const decoded = req.decoded;
+            console.log(decoded.email);
+            if(decoded.email !== req.query.email){
+            return res.status(401).send({message: 'unauthorized access'})
+            }
+            const email = req.query.email;
             const query = {email}
-            const cursor = reviewCollection.find(query)
+            const cursor = reviewCollection.find(query).sort({$natural:-1})
             const review = await cursor.toArray()
             res.send(review)
         }) 
